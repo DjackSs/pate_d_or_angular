287df1dbb10320494007936301647f65a2d6936f
Pierreoliviermpo <po.mpolesha@gmail.com>
2024-03-26 18:45:58 +0100

    addind provideHttpClient with Injector in appConfig

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,16 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, withInterceptors } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { tokenInterceptor } from './interceptor/token';
 
 export const appConfig: ApplicationConfig = {
-  providers: 
-  [
+  providers: [
     provideRouter(routes),
-    importProvidersFrom(HttpClientModule)
-  ]
-
+    importProvidersFrom(HttpClientModule),
+    provideHttpClient(withInterceptors([tokenInterceptor])),
+  ],
 };
