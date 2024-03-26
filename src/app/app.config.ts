cbf79cb43e51d8734192647b4daf931a007e39ee
Julien Bisson <julien.bisson2@gmail.com>
2024-03-26 16:35:59 +0100

    fix merging from branch dev

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,23 @@
-import { HttpClientModule } from '@angular/common/http';
+import {
+  HttpClientModule,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import player from 'lottie-web';
 import { provideLottieOptions } from 'ngx-lottie';
 import { routes } from './app.routes';
+import { tokenInterceptor } from './interceptor/token';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),
+  providers: [
+    provideRouter(routes),
     importProvidersFrom(HttpClientModule),
-  provideLottieOptions({
-    player: () => player,
-  })]
+    provideLottieOptions({
+      player: () => player,
+    }),
+    provideHttpClient(withInterceptors([tokenInterceptor])),
+  ],
 };
