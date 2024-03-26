e9cc262bbbc98962a440b1e9ecd3c13c13eba935
Julien Bisson <julien.bisson2@gmail.com>
2024-03-26 16:44:12 +0100

    changes directory

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoaderComponent } from './component/loader/loader/loader.component';
+import { LoaderComponent } from './components/loader/loader.component';
 
 
 
