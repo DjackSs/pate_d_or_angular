9041edc33c9be2c4194be24c8f35e0341a3436b2
Pierreoliviermpo <po.mpolesha@gmail.com>
2024-03-27 02:03:34 +0100

    Google Fonts, custom/theme-colors, RestaurantsComponent CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoaderComponent } from './components/loader/loader.component';
 
